60442acec0ccca37c53b9d4fbd2ac1323c78693f
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-17 14:59:07 +0200

    MILESTONE 2: COMPLETATA
    con postman ho testato l’endpoint e ho visto che l’API mi restituisce un array di oggetti JSON, ognuno con queste proprietà:
    
    id: identificativo numerico della foto
    
    title: il titolo della foto
    
    date: la data associata
    
    url: il link diretto all’immagine

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+/*
+Milestone 2:
+Utilizzando Postman, testiamo una chiamata a questo endpoint: 
 
+https://lanciweb.github.io/demo/api/pictures/
+
+Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come poterli sfruttare.
+
+______________________________________________________________
+Milestone 3:
+Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+*/
