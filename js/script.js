3bddf0fbfaae3e8dd07d9de8f874c30f3b64034d
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-17 15:34:36 +0200

    iniziato a lavorare su un foglio js:
    effettuato selezione elementi di output,
    
    salvato in una costante l'end point dell'api
    
    effettuato chiamata ajax all'api ed estrapolato i primi dati necessari per poter proseguire con la richiesta finale dell'esercizio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /*
+______________________________________________________________
 Milestone 2:
 Utilizzando Postman, testiamo una chiamata a questo endpoint: 
 
@@ -9,4 +10,23 @@ Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come po
 ______________________________________________________________
 Milestone 3:
 Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+______________________________________________________________
 */
+
+//selezione degli elementi di output
+const cardsContainer = document.querySelector(".cards-container");
+    //console.log(cardsContainer);
+
+//salvo in una costante l’endpoint dell’API
+const apiRef = "https://lanciweb.github.io/demo/api/pictures/";
+
+//chiamata ajax all'api
+axios.get(apiRef).then((response) => {
+
+    //console.log(response);
+
+    const posts = response.data;
+    console.log(posts); // --> estrapolati i dati necessari
+    
+    
+})
